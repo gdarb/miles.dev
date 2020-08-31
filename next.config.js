c9ffbf39de036f53b44e1a304b87f6228a66126d
Aidan Miles <aidan.miles@gmail.com>
2020-08-31 16:23:52 +1000

    feat: add default HTTP headers
    * X-Content-Type-Options
    * X-XSS-Protection
    * X-Frame-Options
    * Referrer-Policy
    * Feature-Policy
    * Report-To
    * NEL

--- next.config.js
+++ next.config.js
@@ -1,3 +1,40 @@
 module.exports = {
 	reactStrictMode: true,
+	async headers() {
+		return [
+			{
+				source: "/(.*)",
+				headers: [
+					{
+						key: "X-Content-Type-Options",
+						value: "nosniff",
+					},
+					{
+						key: "X-XSS-Protection",
+						value: "1; mode=block",
+					},
+					{
+						key: "X-Frame-Options",
+						value: "DENY",
+					},
+					{
+						key: "Referrer-Policy",
+						value: "strict-origin-when-cross-origin",
+					},
+					{
+						key: "Feature-Policy",
+						value: `accelerometer 'none'; camera 'none'; geolocation 'none'; gyroscope 'none'; magnetometer 'none'; microphone 'none'; payment 'none'; usb 'none'`,
+					},
+					{
+						key: "Report-To",
+						value: `{"group":"default","max_age":31536000,"endpoints":[{"url":"https://gdarb.report-uri.com/a/d/g"}],"include_subdomains":true}`,
+					},
+					{
+						key: "NEL",
+						value: `{"report_to":"default","max_age":31536000,"include_subdomains":true}`,
+					},
+				],
+			},
+		];
+	},
 };
