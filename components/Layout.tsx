a2d7e949a4252275dd2078f555495d7341f3e2e4
Aidan Miles <aidan.miles@gmail.com>
2021-04-15 13:57:13 +1000

    feat: add `SEO` component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,17 +1,21 @@
 import {ReactNode} from "react";
 import {Header} from "./Header";
 import {Footer} from "./Footer";
+import {SEO} from "./SEO";
 
 type LayoutProps = {
 	children: ReactNode;
 };
 
 export function Layout({children}: LayoutProps) {
-	return <div className="flex flex-col min-h-screen">
-		<Header />
-		<main className="flex flex-auto">
-			{children}
-		</main>
-		<Footer />
-	</div>;
+	return <>
+		<SEO />
+		<div className="flex flex-col min-h-screen">
+			<Header />
+			<main className="flex flex-auto">
+				{children}
+			</main>
+			<Footer />
+		</div>
+	</>;
 }
