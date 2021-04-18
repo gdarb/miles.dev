c155085ffacbfb14b100c179557840c2718f24b1
Aidan Miles <aidan.miles@gmail.com>
2021-04-18 15:34:42 +1000

    feat: add `Hover` component

--- components/Anchor.tsx
+++ components/Anchor.tsx
@@ -1,18 +1,14 @@
-import clsx from "clsx";
 import {ComponentPropsWithoutRef, forwardRef} from "react";
+import {Hover} from "./Hover";
 
 export const Anchor = forwardRef<
 	HTMLAnchorElement,
 	ComponentPropsWithoutRef<"a">
->(({children, className, href, ...rest}, ref) =>
+>(({href, children, ...rest}, ref) =>
 // rome-ignore lint/jsx/noPropSpreading: just a wrapper around <a>
-	<a className={clsx(
-		"transition duration-300 hover:text-blue-600 dark:hover:text-blue-400",
-		className,
-	)}
-	href={href}
-	ref={ref}
-	{...rest}>
-		{children}
+	<a ref={ref} href={href} {...rest}>
+		<Hover>
+			{children}
+		</Hover>
 	</a>
 );
