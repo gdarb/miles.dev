fbe11128c70ff588de23374cace713d9e766b64b
Aidan Miles <aidan.miles@gmail.com>
2021-09-04 20:29:56 +1000

    fix: remove `rome-ignore` comments

--- components/Anchor.tsx
+++ components/Anchor.tsx
@@ -5,7 +5,6 @@ export const Anchor = forwardRef<
 	HTMLAnchorElement,
 	ComponentPropsWithoutRef<"a">
 >(({ href, children, ...rest }, ref) => (
-	// rome-ignore lint/jsx/noPropSpreading: just a wrapper around <a>
 	<a ref={ref} href={href} {...rest}>
 		<Hover>{children}</Hover>
 	</a>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import { ThemeProvider } from "next-themes";
 import "tailwindcss/tailwind.css";
 
 function app({ Component, pageProps }: AppProps) {
-	// rome-ignore lint/jsx/noPropSpreading: required by next
 	return (
 		<ThemeProvider attribute="class" disableTransitionOnChange={true}>
 			<Component {...pageProps} />
