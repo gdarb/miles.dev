c7d28cc84a6d96ebbc9d0e0189c3a0cfc563026b
Aidan Miles <aidan.miles@gmail.com>
2021-09-04 20:46:59 +1000

    fix(anchor): ensure a `displayName` is assigned

--- components/Anchor.tsx
+++ components/Anchor.tsx
@@ -4,8 +4,10 @@ import { Hover } from "./Hover";
 export const Anchor = forwardRef<
 	HTMLAnchorElement,
 	ComponentPropsWithoutRef<"a">
->(({ href, children, ...rest }, ref) => (
-	<a ref={ref} href={href} {...rest}>
-		<Hover>{children}</Hover>
-	</a>
-));
+>(function Anchor({ href, children, ...rest }, ref) {
+	return (
+		<a ref={ref} href={href} {...rest}>
+			<Hover>{children}</Hover>
+		</a>
+	);
+});
