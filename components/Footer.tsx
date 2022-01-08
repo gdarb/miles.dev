97c26137b3b8e02cbf91e8a449922c7fd7d5305e
Aidan Miles <aidan.miles@gmail.com>
2022-01-08 17:07:27 +1100

    feat(footer): remove content

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,5 @@
 export function Footer() {
 	return (
-		<footer className="flex justify-center py-6 text-sm font-light">
-			&copy; 2021
-		</footer>
+		<footer className="flex justify-center py-6 text-sm font-light"></footer>
 	);
 }
